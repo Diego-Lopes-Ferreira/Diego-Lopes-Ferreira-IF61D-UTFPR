45f5a2dd9812a2a3d63bd54e12ddcca18ac8db80
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-09-07 16:36:38 -0300

    feat(aula02): ex19_.c

--- Aula 02 - Estruturas de Seleção - Exemplos/mine/11-20/ex19_.c
+++ Aula 02 - Estruturas de Seleção - Exemplos/mine/11-20/ex19_.c
@@ -1,17 +1,27 @@
 /*
   Calcular o dia da semana de uma data entre 1 de março de 1700 e 28 de
-  fevereiro de 2100, utilizando o seguinte método: n = (int)(365.25 * g) +
-  (int)(30.6 * f) - 621049 + d; ds = round( frac(n/7) * 7) + delta + 1;
+  fevereiro de 2100, utilizando o seguinte método:
+  n = (int)(365.25 * g) + (int)(30.6 * f) - 621049 + d;
+  ds = round( frac(n/7) *7) + delta + 1;
     // sendo:
     d => dia
     m => mes
     a => ano
-    ds => dia da semana indexOf - [segunda, terca, quarta, quinta, sexta,
-  sabado, domingo]
+    ds => dia da semana [sabado, domingo, segunda, terca, quarta, quinta, sexta]
 
-    g = m <= 2 ? a-1 : a;
-    f = m <= 2 ? m+13 : m+1;
-    delta = {
+    float calcG(float a){
+      if (a <= 2)
+        return a - 1;
+      else
+        return a;
+    }
+    float calcF(float m){
+      if (m <= 2)
+        return m+13;
+      else
+        return m+1;
+    }
+    float calcDelta () = {
       if (n < 36523)
        return 2;
       if (n < 73048)
@@ -27,11 +37,71 @@
   processamento:
 
 */
+#include <math.h>
 #include <stdio.h>
 
-float frac
+float calcG(float a) {
+  if (a <= 2)
+    return a - 1;
+  else
+    return a;
+}
+float calcF(float m) {
+  if (m <= 2)
+    return m + 13;
+  else
+    return m + 1;
+}
+float calcDelta(float n) {
+  if (n < 36523)
+    return 2;
+  else if (n < 73048)
+    return 1;
+  else
+    return 0;
+}
+float calcFrac(float x) {
+  float floor, final;
+  floor = floorf(x);
+  final = x - floor;
+  return final;
+}
 
-    int
-    main() {
+int main() {
+  float d, m, a, ds;
+  float g, f, delta, n;
+
+  printf(
+      "Digite a data no formato: DD/MM/AAAA (com as barras) \nDD/MM/AAAA>>> ");
+  scanf("%f/%f/%f", &d, &m, &a);
+  printf("Dia: %.2f Mes: %.2f Ano: %.2f \n", d, m, a);
+
+  g = calcG(a);
+  printf("G: %.2f \n", g);
+  f = calcF(m);
+  printf("F: %.2f \n", f);
+  n = (float)((int)(365.25 * g) + (int)(30.6 * f) - 621049 + d);
+  printf("N: %.2f \n", n);
+
+  delta = calcDelta(n);
+  printf("Delta: %.2f \n", delta);
+  ds = round(calcFrac(n / 7) * 7) + delta + 1;
+  printf("Ds: %.2f \n", ds);
+
+  if (ds == 0) {
+    printf("Sabado \n");
+  } else if (ds == 1) {
+    printf("Domingo \n");
+  } else if (ds == 2) {
+    printf("Segunda \n");
+  } else if (ds == 3) {
+    printf("Terca \n");
+  } else if (ds == 4) {
+    printf("Quarta \n");
+  } else if (ds == 5) {
+    printf("Quinta \n");
+  } else if (ds == 6) {
+    printf("Sexta \n");
+  }
   return 0;
 }
